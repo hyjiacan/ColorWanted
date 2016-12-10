8fafeb15d96e655163e486e1a9bff753d01bd963
hyjiacan <me@hyjiacan.com>
2016-12-10 17:39:28 +0800

    优化 在Alt+Tab列表中隐藏取色窗口

--- ColorWanted/MainForm.cs
+++ ColorWanted/MainForm.cs
@@ -51,7 +51,18 @@ namespace ColorWanted
             Util.BindHotkeys(Handle);
         }
 
-
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int WS_EX_APPWINDOW = 0x40000;
+                const int WS_EX_TOOLWINDOW = 0x80;
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle &= (~WS_EX_APPWINDOW);    // 不显示在TaskBar
+                cp.ExStyle |= WS_EX_TOOLWINDOW;      // 不显示在Alt-Tab
+                return cp;
+            }
+        }
 
         private void timer_Tick(object sender, EventArgs e)
         {
