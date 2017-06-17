d495b114c3e45921737ce10e9dc1598052bffb58
hyjiacan <me@hyjiacan.com>
2017-06-17 11:27:07 +0800

    移除多余的资源文件

--- ColorWanted/Program.cs
+++ ColorWanted/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using ColorWanted.update;
+using System;
 using System.Threading;
 using System.Windows.Forms;
-using ColorWanted.update;
 
 namespace ColorWanted
 {
